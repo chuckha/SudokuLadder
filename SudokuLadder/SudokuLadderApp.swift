44714d60f2cb18d46367479cf79390e91c38765b
chuckha <chuckha@hey.com>
2024-01-17 23:30:41 -0800

    Initial Commit

---
+++ SudokuLadder/SudokuLadderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuLadderApp.swift
+//  SudokuLadder
+//
+//  Created by chuck ha on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SudokuLadderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
