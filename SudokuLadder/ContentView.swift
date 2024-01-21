00f771f5791b6a17c5bc23a28630e2834c7db977
chuckha <chuckha@hey.com>
2024-01-21 12:04:24 -0800

    swift-format

--- SudokuLadder/ContentView.swift
+++ SudokuLadder/ContentView.swift
@@ -7,38 +7,47 @@
 
 import SwiftUI
 
-let normalSudoku: [Dictionary<String,Int>] = [
-    c(0,0,0), c(0,1,0), c(0,2,0), c(0,3,1), c(0,4,1), c(0,5,1), c(0,6,2), c(0,7,2), c(0,8,2),
-    c(1,0,0), c(1,1,0), c(1,2,0), c(1,3,1), c(1,4,1), c(1,5,1), c(1,6,2), c(1,7,2), c(1,8,2),
-    c(2,0,0), c(2,1,0), c(2,2,0), c(2,3,1), c(2,4,1), c(2,5,1), c(2,6,2), c(2,7,2), c(2,8,2),
-    c(3,0,3), c(3,1,3), c(3,2,3), c(3,3,4), c(3,4,4), c(3,5,4), c(3,6,5), c(3,7,5), c(3,8,5),
-    c(4,0,3), c(4,1,3), c(4,2,3), c(4,3,4), c(4,4,4), c(4,5,4), c(4,6,5), c(4,7,5), c(4,8,5),
-    c(5,0,3), c(5,1,3), c(5,2,3), c(5,3,4), c(5,4,4), c(5,5,4), c(5,6,5), c(5,7,5), c(5,8,5),
-    c(6,0,6), c(6,1,6), c(6,2,6), c(6,3,7), c(6,4,7), c(6,5,7), c(6,6,8), c(6,7,8), c(6,8,8),
-    c(7,0,6), c(7,1,6), c(7,2,6), c(7,3,7), c(7,4,7), c(7,5,7), c(7,6,8), c(7,7,8), c(7,8,8),
-    c(8,0,6), c(8,1,6), c(8,2,6), c(8,3,7), c(8,4,7), c(8,5,7), c(8,6,8), c(8,7,8), c(8,8,8)
+let normalSudoku: [[String: Int]] = [
+  c(0, 0, 0), c(0, 1, 0), c(0, 2, 0), c(0, 3, 1), c(0, 4, 1), c(0, 5, 1), c(0, 6, 2), c(0, 7, 2),
+  c(0, 8, 2),
+  c(1, 0, 0), c(1, 1, 0), c(1, 2, 0), c(1, 3, 1), c(1, 4, 1), c(1, 5, 1), c(1, 6, 2), c(1, 7, 2),
+  c(1, 8, 2),
+  c(2, 0, 0), c(2, 1, 0), c(2, 2, 0), c(2, 3, 1), c(2, 4, 1), c(2, 5, 1), c(2, 6, 2), c(2, 7, 2),
+  c(2, 8, 2),
+  c(3, 0, 3), c(3, 1, 3), c(3, 2, 3), c(3, 3, 4), c(3, 4, 4), c(3, 5, 4), c(3, 6, 5), c(3, 7, 5),
+  c(3, 8, 5),
+  c(4, 0, 3), c(4, 1, 3), c(4, 2, 3), c(4, 3, 4), c(4, 4, 4), c(4, 5, 4), c(4, 6, 5), c(4, 7, 5),
+  c(4, 8, 5),
+  c(5, 0, 3), c(5, 1, 3), c(5, 2, 3), c(5, 3, 4), c(5, 4, 4), c(5, 5, 4), c(5, 6, 5), c(5, 7, 5),
+  c(5, 8, 5),
+  c(6, 0, 6), c(6, 1, 6), c(6, 2, 6), c(6, 3, 7), c(6, 4, 7), c(6, 5, 7), c(6, 6, 8), c(6, 7, 8),
+  c(6, 8, 8),
+  c(7, 0, 6), c(7, 1, 6), c(7, 2, 6), c(7, 3, 7), c(7, 4, 7), c(7, 5, 7), c(7, 6, 8), c(7, 7, 8),
+  c(7, 8, 8),
+  c(8, 0, 6), c(8, 1, 6), c(8, 2, 6), c(8, 3, 7), c(8, 4, 7), c(8, 5, 7), c(8, 6, 8), c(8, 7, 8),
+  c(8, 8, 8),
 ]
 
-func c(_ row: Int,_ col: Int,_ box: Int) -> Dictionary<String,Int> {
-    return ["row":row,"col":col,"box":box]
+func c(_ row: Int, _ col: Int, _ box: Int) -> [String: Int] {
+  return ["row": row, "col": col, "box": box]
 }
 
-func layoutToSuduoku(_ dic: [Dictionary<String,Int>]) -> Sudoku {
-    let cells = dic.map{ (entry) -> Cell in
-        Cell(box: entry["box"]!, row: entry["row"]!, col: entry["col"]!)
-    }
-    var sudokuCells: [[Cell]] = []
+func layoutToSuduoku(_ dic: [[String: Int]]) -> Sudoku {
+  let cells = dic.map { (entry) -> Cell in
+    Cell(box: entry["box"]!, row: entry["row"]!, col: entry["col"]!)
+  }
+  var sudokuCells: [[Cell]] = []
+  for _ in 0..<9 {
+    var row: [Cell] = []
     for _ in 0..<9 {
-        var row: [Cell] = []
-        for _ in 0..<9 {
-            row.append(Cell(box:0,row:0,col:0))
-        }
-        sudokuCells.append(row)
+      row.append(Cell(box: 0, row: 0, col: 0))
     }
-    for cell in cells {
-        sudokuCells[cell.row][cell.col] = cell
-    }
-    return Sudoku(cells: sudokuCells)
+    sudokuCells.append(row)
+  }
+  for cell in cells {
+    sudokuCells[cell.row][cell.col] = cell
+  }
+  return Sudoku(cells: sudokuCells)
 }
 
 // - [x] add borders to cells
@@ -46,46 +55,48 @@ func layoutToSuduoku(_ dic: [Dictionary<String,Int>]) -> Sudoku {
 // - [] edit value
 
 struct ContentView: View {
-    @StateObject private var sudoku: SudokuViewModelV2 = SudokuViewModelV2(sudoku: layoutToSuduoku(normalSudoku))
+  @StateObject private var sudoku: SudokuViewModelV2 = SudokuViewModelV2(
+    sudoku: layoutToSuduoku(normalSudoku))
 
-    var body: some View {
-        VStack{
-            Grid(horizontalSpacing: 0,verticalSpacing: 0) {
-                ForEach(sudoku.cells, id: \.self) { row in
-                    GridRow{
-                        ForEach(row, id: \.self) { cell in
-                            CellView(cell: cell)
-                        }
-                    }
-                }
+  var body: some View {
+    VStack {
+      Grid(horizontalSpacing: 0, verticalSpacing: 0) {
+        ForEach(sudoku.cells, id: \.self) { row in
+          GridRow {
+            ForEach(row, id: \.self) { cell in
+              CellView(cell: cell)
             }
-            .gesture(DragGesture(minimumDistance: 0)
-                .onChanged { value in
-                    // Will there be a bug if the user drags over the same location during a long drag?
-                    if value.startLocation == value.location {
-                        sudoku.clearSelection()
-                    }
-                    sudoku.selectCellFromPoint(at: value.location)
-                })
+          }
         }
+      }
+      .gesture(
+        DragGesture(minimumDistance: 0)
+          .onChanged { value in
+            // Will there be a bug if the user drags over the same location during a long drag?
+            if value.startLocation == value.location {
+              sudoku.clearSelection()
+            }
+            sudoku.selectCellFromPoint(at: value.location)
+          })
     }
- }
+  }
+}
 
 struct CellView: View {
-    @ObservedObject var cell: CellViewModel
-    
-    var body: some View {
-        ZStack{
-            Rectangle()
-                .frame(width: cellWidth, height: cellHeight)
-                .foregroundColor(cell.color())
-                .overlay(
-                    EdgeBorder(color: Color.primary, width: 3.0, edges: cell.boxBorder)
-                        .stroke()
-                )
-            Text(cell.display())
-        }
+  @ObservedObject var cell: CellViewModel
+
+  var body: some View {
+    ZStack {
+      Rectangle()
+        .frame(width: cellWidth, height: cellHeight)
+        .foregroundColor(cell.color())
+        .overlay(
+          EdgeBorder(color: Color.primary, width: 3.0, edges: cell.boxBorder)
+            .stroke()
+        )
+      Text(cell.display())
     }
+  }
 }
 
 //struct NumpadView: View {
@@ -109,23 +120,23 @@ struct CellView: View {
 //}
 
 struct ControlsView: View {
-    @Binding var controlMode: ControlMode
-    
-    var body: some View {
-        HStack{
-            VStack{
-                Button("Big") {
-                    controlMode = .BigNumber
-                }
-                Button("Corner") {
-                    controlMode = .CornerNumber
-                }
-                Button("Middle") {
-                    controlMode = .MiddleNumber
-                }
-            }
+  @Binding var controlMode: ControlMode
+
+  var body: some View {
+    HStack {
+      VStack {
+        Button("Big") {
+          controlMode = .BigNumber
         }
+        Button("Corner") {
+          controlMode = .CornerNumber
+        }
+        Button("Middle") {
+          controlMode = .MiddleNumber
+        }
+      }
     }
+  }
 }
 
 //struct BorderStyle {
@@ -134,37 +145,37 @@ struct ControlsView: View {
 //}
 
 #Preview {
-    ContentView()
+  ContentView()
 }
 
 struct EdgeBorder: Shape {
-    var color: Color
-    var width: CGFloat
-    var edges: Edge.Set
+  var color: Color
+  var width: CGFloat
+  var edges: Edge.Set
 
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        if edges.contains(.top) {
-            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        }
-        if edges.contains(.leading) {
-            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        }
-        if edges.contains(.bottom) {
-            path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        }
-        if edges.contains(.trailing) {
-            path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        }
-        return path
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+    if edges.contains(.top) {
+      path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+      path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
     }
-    
-    func stroke() -> some View {
-        self.stroke(color, style: StrokeStyle(lineWidth: width))
+    if edges.contains(.leading) {
+      path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+      path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
     }
+    if edges.contains(.bottom) {
+      path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+      path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+    }
+    if edges.contains(.trailing) {
+      path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+      path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+    }
+    return path
+  }
+
+  func stroke() -> some View {
+    self.stroke(color, style: StrokeStyle(lineWidth: width))
+  }
 
 }
--- SudokuLadder/SudokuViewModel.swift
+++ SudokuLadder/SudokuViewModel.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-let cellWidth : CGFloat = 40
-let cellHeight : CGFloat = 40
+let cellWidth: CGFloat = 40
+let cellHeight: CGFloat = 40
 
 //class SudokuViewModel: ObservableObject {
 ////    @Published var board: Sudoku
 //    @Published var currentMode: ControlMode = .BigNumber
 //    @Published var cells: [[CellViewModel]]
-//    
+//
 //    var sudoku: Sudoku
 //
 //    init(sudoku: Sudoku) {
@@ -83,161 +83,166 @@ let cellHeight : CGFloat = 40
 // SudokuViewModel is the model that layers UI concerns on top of sudoku.
 // What are the UI concerns of sudoku? How big it is? No. Where the borders are? Yes!
 class SudokuViewModelV2: ObservableObject {
-    private var sudoku: Sudoku
-    @Published var cells: [[CellViewModel]]
-    
-    init(sudoku: Sudoku) {
-        self.sudoku = sudoku
-        cells = sudoku.cells.map { (row) -> [CellViewModel] in
-            row.map{ (cell) -> CellViewModel in
-                CellViewModel(cell: cell)
-            }
-        }
-        // box border gets calculated once
-        // for each cell, look at all neighbors,
-        // if the neighbor is in the same box, do not set the border
-        // if the neighbor is not in the same box, add a border
-        // if the value is outside the bounds, add a border
-        cells.forEach{ (row) in
-            row.forEach{ (cell) in
-                var top = false; var leading = false; var bottom = false; var trailing = false;
-                // top
-                if cell.row() > 0 {
-                    if cells[cell.row()-1][cell.col()].box() != cell.box() {
-                        top = true
-                    }
-                }
-                if cell.row() == 0 {
-                    top = true
-                }
-                // trailing
-                if cell.col() < self.sudoku.width-1 {
-                    if cells[cell.row()][cell.col()+1].box() != cell.box() {
-                        trailing = true
-                    }
-                }
-                if cell.col() == self.sudoku.width-1 {
-                    trailing = true
-                }
-                // bottom
-                if cell.row() < self.sudoku.height-1 {
-                    if cells[cell.row()+1][cell.col()].box() != cell.box() {
-                        bottom = true
-                    }
-                }
-                if cell.row() == self.sudoku.height-1 {
-                    bottom = true
-                }
-                // leading
-                if cell.col() > 0 {
-                    if cells[cell.row()][cell.col()-1].box() != cell.box() {
-                        leading = true
-                    }
-                }
-                if cell.col() == 0 {
-                    leading = true
-                }
-                
-                cell.setBoxBorder(top: top, leading: leading, bottom: bottom, trailing: trailing)
-                print("b:\(cell.box()),\(cell.row()),\(cell.col())", cell.boxBorder)
-            }
-        }
-        
-    }
-    convenience init() {
-        self.init(sudoku: Sudoku())
-    }
+  private var sudoku: Sudoku
+  @Published var cells: [[CellViewModel]]
 
-    func selectCell(rowidx: Int, colidx: Int) {
-        cells[rowidx][colidx].select()
+  init(sudoku: Sudoku) {
+    self.sudoku = sudoku
+    cells = sudoku.cells.map { (row) -> [CellViewModel] in
+      row.map { (cell) -> CellViewModel in
+        CellViewModel(cell: cell)
+      }
     }
-    
-    func selectCellFromPoint(at point: CGPoint) {
-        let row = Int(point.y / cellWidth)
-        let column = Int(point.x / cellHeight)
-        if row >= 0 && row < sudoku.height && column >= 0 && column < sudoku.width {
-            selectCell(rowidx: row, colidx: column)
+    // box border gets calculated once
+    // for each cell, look at all neighbors,
+    // if the neighbor is in the same box, do not set the border
+    // if the neighbor is not in the same box, add a border
+    // if the value is outside the bounds, add a border
+    cells.forEach { (row) in
+      row.forEach { (cell) in
+        var top = false
+        var leading = false
+        var bottom = false
+        var trailing = false
+        // top
+        if cell.row() > 0 {
+          if cells[cell.row() - 1][cell.col()].box() != cell.box() {
+            top = true
+          }
         }
-    }
-    
-    func clearSelection() {
-        for (i, cellRow) in cells.enumerated() {
-            for (j, _) in cellRow.enumerated(){
-                cells[i][j].selected = false
-            }
+        if cell.row() == 0 {
+          top = true
+        }
+        // trailing
+        if cell.col() < self.sudoku.width - 1 {
+          if cells[cell.row()][cell.col() + 1].box() != cell.box() {
+            trailing = true
+          }
+        }
+        if cell.col() == self.sudoku.width - 1 {
+          trailing = true
+        }
+        // bottom
+        if cell.row() < self.sudoku.height - 1 {
+          if cells[cell.row() + 1][cell.col()].box() != cell.box() {
+            bottom = true
+          }
+        }
+        if cell.row() == self.sudoku.height - 1 {
+          bottom = true
+        }
+        // leading
+        if cell.col() > 0 {
+          if cells[cell.row()][cell.col() - 1].box() != cell.box() {
+            leading = true
+          }
+        }
+        if cell.col() == 0 {
+          leading = true
         }
 
+        cell.setBoxBorder(top: top, leading: leading, bottom: bottom, trailing: trailing)
+        print("b:\(cell.box()),\(cell.row()),\(cell.col())", cell.boxBorder)
+      }
     }
+
+  }
+  convenience init() {
+    self.init(sudoku: Sudoku())
+  }
+
+  func selectCell(rowidx: Int, colidx: Int) {
+    cells[rowidx][colidx].select()
+  }
+
+  func selectCellFromPoint(at point: CGPoint) {
+    let row = Int(point.y / cellWidth)
+    let column = Int(point.x / cellHeight)
+    if row >= 0 && row < sudoku.height && column >= 0 && column < sudoku.width {
+      selectCell(rowidx: row, colidx: column)
+    }
+  }
+
+  func clearSelection() {
+    for (i, cellRow) in cells.enumerated() {
+      for (j, _) in cellRow.enumerated() {
+        cells[i][j].selected = false
+      }
+    }
+
+  }
 }
 
 // CellViewModel is the model that layers UI concerns on top of cells.
-class CellViewModel: ObservableObject,Hashable {
+class CellViewModel: ObservableObject, Hashable {
 
-    let id = UUID()
-    private var cell: Cell
-    @Published var boxBorder: Edge.Set = []
-    
-    @Published var top: Bool = false
-    @Published var leading: Bool = false
-    @Published var bottom: Bool = false
-    @Published var trailing: Bool = false
-    @Published var selected: Bool = false
-    
-    let selectedColor = Color(red: 0.2, green: 0.2, blue: 0.6, opacity: 0.8)
-    let defaultColor = Color(red: 0.8, green: 0.8, blue: 0.8)
-    
-    init(cell: Cell) {
-        self.cell = cell
-    }
-    
-    func color() -> Color {
-        if selected {
-            return selectedColor
-        }
-        return defaultColor
-    }
-    
-    func display() -> String {
-        cell.value?.description ?? "9"
-    }
-    
-    func select() {
-        selected = true
-    }
-    
-    func row() -> Int { return cell.row }
-    func col() -> Int { return cell.col }
-    func box() -> Int { return cell.box }
+  let id = UUID()
+  private var cell: Cell
+  @Published var boxBorder: Edge.Set = []
 
-    func setBoxBorder(top: Bool = false, leading: Bool = false, bottom: Bool = false, trailing: Bool = false) {
-        if top {
-            boxBorder.insert(.top)
-        }
-        if leading {
-            boxBorder.insert(.leading)
-        }
-        if bottom {
-            boxBorder.insert(.bottom)
-        }
-        if trailing {
-            boxBorder.insert(.trailing)
-        }
+  @Published var top: Bool = false
+  @Published var leading: Bool = false
+  @Published var bottom: Bool = false
+  @Published var trailing: Bool = false
+  @Published var selected: Bool = false
+
+  let selectedColor = Color(red: 0.2, green: 0.2, blue: 0.6, opacity: 0.8)
+  let defaultColor = Color(red: 0.8, green: 0.8, blue: 0.8)
+
+  init(cell: Cell) {
+    self.cell = cell
+  }
+
+  func color() -> Color {
+    if selected {
+      return selectedColor
     }
-    
-    // Equatable
-    static func == (lhs: CellViewModel, rhs: CellViewModel) -> Bool {
-        return lhs.id == rhs.id
+    return defaultColor
+  }
+
+  func display() -> String {
+    cell.value?.description ?? "9"
+  }
+
+  func select() {
+    selected = true
+  }
+
+  func row() -> Int { return cell.row }
+  func col() -> Int { return cell.col }
+  func box() -> Int { return cell.box }
+
+  func setBoxBorder(
+    top: Bool = false, leading: Bool = false, bottom: Bool = false, trailing: Bool = false
+  ) {
+    if top {
+      boxBorder.insert(.top)
     }
-    
-    // Hashable
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(cell.row)
-        hasher.combine(cell.col)
+    if leading {
+      boxBorder.insert(.leading)
     }
+    if bottom {
+      boxBorder.insert(.bottom)
+    }
+    if trailing {
+      boxBorder.insert(.trailing)
+    }
+  }
+
+  // Equatable
+  static func == (lhs: CellViewModel, rhs: CellViewModel) -> Bool {
+    return lhs.id == rhs.id
+  }
+
+  // Hashable
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(cell.row)
+    hasher.combine(cell.col)
+  }
 }
 
 enum ControlMode {
-    case BigNumber
-    case CornerNumber
-    case MiddleNumber
+  case BigNumber
+  case CornerNumber
+  case MiddleNumber
 }
